6733577839000207f394934c453ea76791c1e770
sebastian molina <cirp8918@gmail.com>
2026-03-23 12:00:13 -0300

    style: standardize quotation marks in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,7 +14,7 @@ async function bootstrap() {
       },
     }),
   );
-  app.setGlobalPrefix('api/v2');
-  await app.listen(process.env.PORT ?? 3000);
+  app.setGlobalPrefix("api/v2");
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
